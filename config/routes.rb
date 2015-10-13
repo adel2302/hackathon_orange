6587db262641badf3c29cc2513e2df6998dec816
Adel Chouanine <a.chouanine@hotmail.fr>
2015-10-13 15:13:40 +0200

    added users and posts show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+   devise_for :users, :controllers => {registrations: 'registrations'}
+
   resources :infos
   resources :posts do
     collection do
       post 'bygeocode'
     end
   end
-  resources :profile
-
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  resources :users
+ 
   root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
